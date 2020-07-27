fd4cce215c89f13c14aee334d8e81382698dc993
Mehrnaz <mcharkhchi@ebi.ac.uk>
2020-07-27 22:02:10 +0430

    update sequence test file.

--- app/tests/test_api/test_resources/test_sequence.py
+++ app/tests/test_api/test_resources/test_sequence.py
@@ -8,13 +8,13 @@ from main import app
 class APITestCase(unittest.TestCase):
     def setUp(self):
         self.client = TestClient(app)
-        self.api_prefix = 'api'
+        self.api_prefix = "api"
         self.sequence_url_prefix = "api/sequence/"
         self.metadata_url_prefix = "/metadata/"
         self.checksum = "6681ac2f62509cfc220d78751b8dc524"
-        self.refget_url = 'http://hx-rke-wp-webadmin-14-worker-1.caas.ebi.ac.uk:31136/'
+        self.refget_url = "http://hx-rke-wp-webadmin-14-worker-1.caas.ebi.ac.uk:31136/"
         self.sequence_path = self.sequence_url_prefix + self.checksum
-        self.sequence_not_found_path = self.sequence_url_prefix + '6681ac2f62509cfc220d78751b8dc845'
+        self.sequence_not_found_path = self.sequence_url_prefix + "6681ac2f62751b8dc845"
 
     def test_404_error_in_none_relative_requests(self):
         response = self.client.get("api/")
@@ -22,7 +22,7 @@ class APITestCase(unittest.TestCase):
         assert response.status_code == 404
 
     def test_api_error_404(self):
-        get_response = self.client.get(self.sequence_path)
+        get_response = self.client.get(self.sequence_not_found_path)
         assert get_response.status_code == 404
 
     def test_api_error_405(self):
