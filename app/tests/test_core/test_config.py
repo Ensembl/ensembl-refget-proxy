f3905538ce2d888001ced88ed3289521b004488c
Mehrnaz Charkhchi <mcharkhchi@ebi.ac.uk>
2020-08-21 12:55:20 +0000

    Merge dev to master.

---
+++ app/tests/test_core/test_config.py
@@ -0,0 +1,16 @@
+import unittest
+
+from starlette.datastructures import CommaSeparatedStrings
+
+from core import config
+
+
+class ConfigTestCase(unittest.TestCase):
+    def test_environment_variables(self):
+        assert type(config.ALLOWED_HOSTS) == CommaSeparatedStrings
+        assert type(config.REFGET_SERVER_URL_LIST) == list
+        assert len(config.REFGET_SERVER_URL_LIST) > 0
+
+
+if __name__ == "__main__":
+    unittest.main()
