f0d2669ebb4b6ff393fdef082a9e708f1f7f8ab7
Mehrnaz Charkhchi <mcharkhchi@ebi.ac.uk>
2021-07-12 15:49:42 +0100

    debug test

--- app/tests/test_api/test_resources/test_sequence.py
+++ app/tests/test_api/test_resources/test_sequence.py
@@ -40,7 +40,7 @@ class APISequenceTestCase(unittest.TestCase):
 
     def test_api_error_404(self):
         get_response = self.client.get(self.sequence_not_found_path)
-        logger.log("DEBUG", metadata_url_list(get_response.status_code))
+        logger.log("DEBUG", get_response)
 
         assert get_response.status_code == 404
 
